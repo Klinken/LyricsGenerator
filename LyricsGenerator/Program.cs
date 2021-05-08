a67c30bf0660684b7e6abb1088629173295fa69b
Soren <klinkwort92@gmail.com>
2021-05-08 17:39:12 +0200

    initial commit

---
+++ LyricsGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LyricsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DATA
+
+            // METHODS
+
+            // EXECUTION
+
+
+        }
+    }
+}
